66c618cac2f3eef2dfb590f2a7eb0127560730a8
uasan <ua.san.alex@gmail.com>
2025-02-06 00:36:23 +0100

    injectSQL

--- src/runtime/postgres/model.js
+++ src/runtime/postgres/model.js
@@ -4,7 +4,7 @@ import { Patch } from './patch/Patch.js';
 export class TableModel {
   static relations = nullObject;
 
-  static toSQL({ source }, string) {
+  static injectSQL({ source }, string) {
     source[source.length - 1] += this.table.name + string;
   }
 
